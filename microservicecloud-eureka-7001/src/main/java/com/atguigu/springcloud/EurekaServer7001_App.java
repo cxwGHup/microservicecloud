c847ca1669443e7926f9afbd7bfd16c84e84f0e7
CXW <851827168@qq.com>
2019-03-16 10:49:06 +0800

    微服务整体架构搭建

---
+++ microservicecloud-eureka-7001/src/main/java/com/atguigu/springcloud/EurekaServer7001_App.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/*
+* 启动 EurekaServer7001_App
+*   http://localhost:7001/   同Tomcat 主界面
+* */
+@SpringBootApplication
+@EnableEurekaServer // EurekaServer服务器端启动类,接受其它微服务注册进来
+public class EurekaServer7001_App
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(EurekaServer7001_App.class, args);
+    }
+}
---
+++ sql/microservicecloud.sql
@@ -0,0 +1,7 @@
+create table dept
+(
+  deptno    bigint auto_increment
+    primary key,
+  dname     varchar(60) null,
+  db_source varchar(60) null
+);
