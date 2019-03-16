f9385f63d99aa25806a38ac0eebbd40c01481332
CXW <851827168@qq.com>
2019-03-16 17:27:15 +0800

    Eureka 服务 集群搭建

---
+++ microservicecloud-eureka-7002/src/main/java/com/cxw/springcloud/EurekaServer7002_App.java
@@ -0,0 +1,19 @@
+package com.cxw.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/*
+* 启动 EurekaServer7001_App
+*   http://localhost:7001/   同Tomcat 界面 [还未注册服务]
+* */
+@SpringBootApplication
+@EnableEurekaServer // EurekaServer服务器端启动类,接受其它微服务注册进来
+public class EurekaServer7002_App
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(EurekaServer7002_App.class, args);
+    }
+}
