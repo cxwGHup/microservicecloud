afd11e939c7b6bb0a2ee24801f673fbd69515bf4
CXW <851827168@qq.com>
2019-03-16 11:28:29 +0800

    启动 DeptProvider8001_App * 访问 http://localhost:8001/dept/list *      http://localhost:8001/dept/get/1

--- microservicecloud-provider-dept-8001/src/main/java/com/cxw/springcloud/DeptProvider8001_App.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/cxw/springcloud/DeptProvider8001_App.java
@@ -4,7 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-
+/*
+* 启动 DeptProvider8001_App
+* 访问 http://localhost:8001/dept/list
+*      http://localhost:8001/dept/get/1
+* */
 @SpringBootApplication
 @EnableEurekaClient //本服务启动后会自动注册进eureka服务中
 @EnableDiscoveryClient //服务发现
